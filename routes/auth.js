1197581b44359c7cd90f66ad63d1cb5af65b491c
abhishek-aivi <abhishek.porwal@aivi.in>
2025-05-28 01:15:43 +0530

    chore: add POST request to test /register route

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,11 @@ const { body } = require("express-validator");
 const { register, login } = require("../controllers/authController");
 const { validate } = require("../middlewares/validate");
 const router = express.Router();
+
+router.post("/", (req, res) => {
+  res.json({ message: "Register endpoint working" });
+});
+
 router.post(
   "/register",
   validate([
