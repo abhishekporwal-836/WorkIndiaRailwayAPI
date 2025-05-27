6a500335e941d092eeb1744e40a5818f82ce98c2
abhishek-aivi <abhishek.porwal@aivi.in>
2025-05-28 00:28:59 +0530

    fix: enable remote DB connection by updating SSL settings

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,13 @@ require('dotenv').config();
 module.exports = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
+  port: 18172,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 20,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
